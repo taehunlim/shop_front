f2e45c708e01666b34f04d42d3d78be84a78b897
taehunlim <dsa1520@naver.com>
2022-12-19 20:46:13 +0900

    Feat: HoverBanner #2
    add content on banner

--- src/components/organisms/HoverBanner/index.tsx
+++ src/components/organisms/HoverBanner/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import Slide from 'components/molecules/Slide';
-import { Container, BannerContainer } from './style';
+import { Container, BannerContainer, Content } from './style';
 
 function HoverBanner() {
    return (
@@ -12,18 +13,30 @@ function HoverBanner() {
                   src="/assets/images/sub-banner-size.jpg"
                   alt="hover-banner-1"
                />
+               <Content>
+                  <p>Product</p>
+                  <Link to="/">SHOP NOW</Link>
+               </Content>
             </BannerContainer>
             <BannerContainer>
                <img
                   src="/assets/images/sub-banner-size.jpg"
                   alt="hover-banner-2"
                />
+               <Content>
+                  <p>Product</p>
+                  <Link to="/">SHOP NOW</Link>
+               </Content>
             </BannerContainer>
             <BannerContainer>
                <img
                   src="/assets/images/sub-banner-size.jpg"
                   alt="hover-banner-3"
                />
+               <Content>
+                  <p>Product</p>
+                  <Link to="/">SHOP NOW</Link>
+               </Content>
             </BannerContainer>
          </Slide>
       </Container>
--- src/components/organisms/HoverBanner/style.tsx
+++ src/components/organisms/HoverBanner/style.tsx
@@ -1,4 +1,31 @@
 import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+import { ThemeProps } from 'assets/emotion';
+
+const lineStyles = ({ theme }: ThemeProps) => css`
+   position: relative;
+   ::after {
+      content: '';
+      position: absolute;
+
+      left: auto;
+      right: 0;
+      bottom: 0;
+      width: 0;
+      height: 1px;
+
+      transition: 0.3s;
+
+      background-color: ${theme.fg.black};
+   }
+   :hover {
+      ::after {
+         left: 0;
+         width: 100%;
+      }
+   }
+`;
 
 const Container = styled.div`
    height: fit-content;
@@ -8,15 +35,37 @@ const Container = styled.div`
 const BannerContainer = styled.div`
    position: relative;
    overflow: hidden;
+   width: 100%;
+   padding-top: ${(320 / 560) * 100}%;
 
    img {
+      top: 0;
+      position: absolute;
       width: 100%;
       transition: 0.8s;
+   }
 
-      :hover {
+   :hover {
+      img {
          transform: scale(1.2);
       }
    }
 `;
 
-export { Container, BannerContainer };
+const Content = styled.div`
+   position: absolute;
+   left: 10%;
+   top: 50%;
+   transform: translateY(-50%);
+
+   p {
+      font-size: 28px;
+      margin-bottom: 15px;
+   }
+
+   a {
+      ${lineStyles}
+   }
+`;
+
+export { Container, BannerContainer, Content };
